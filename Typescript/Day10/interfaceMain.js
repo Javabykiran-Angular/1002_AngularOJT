6bee023a3c4991d2bba5c6d6169f7147188171c7
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-10-30 16:09:55 +0530

    Day10

---
+++ Typescript/Day10/interfaceMain.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var employeedetails_1 = require("./employeedetails");
+var obj = new employeedetails_1.EmployeeDetails('Sumit', 'Raokhande', 100000, 'DevOps');
+obj.display();
