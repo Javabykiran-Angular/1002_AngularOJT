1b6b782f3956ad45e89801297c7259dd8f3750c2
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-12-22 16:10:05 +0530

    Day28

---
+++ Angular/Day28/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit,OnDestroy {
+  EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+  + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+  incorrect:string='';
+
+  constructor(private service:LoginService,
+              private router:Router) { }
+  
+
+  ngOnInit(): void {
+    this.setImageToDocument();
+  }
+
+  setImageToDocument(){
+      document.body.className='bg_background';
+  }
+
+  onSubmit(f:NgForm){
+
+    let obj={
+      email:f.value.email,
+      password:f.value.password
+    }
+    this.service.login(obj)
+    .subscribe((response:any)=>{
+      console.log(response);
+      if(response.msg==='Valid User'){
+        this.router.navigate(['/home']);
+      }else{
+        this.incorrect=response.msg;
+      }
+    })
+
+  }
+
+
+  ngOnDestroy(): void {
+    document.body.className='';
+  }
+
+}
