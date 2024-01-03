c656cd7466b851081debf526bc541460f77af7a3
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-01-03 15:57:57 +0530

    Day32

---
+++ Day32/http.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  baseUrl:string='http://localhost:8080/api/';
+
+  constructor(private http:HttpClient) { }
+
+
+  getAllRecord(){
+     return (this.http.get(`${this.baseUrl}getallemployee`));
+  }
+
+  getParticularRecordByID(id:any){
+
+    let idParam=new HttpParams();
+    idParam=idParam.append("id",id);
+
+    return (this.http.get(`${this.baseUrl}getemployeebyId`,{
+      params:idParam
+    }));
+
+  }
+
+  getAllCountry(){
+    return (this.http.get(`${this.baseUrl}getallcountry`));
+  }
+
+  saveRecord(obj:any){
+  return (this.http.post(`${this.baseUrl}addemployee`,obj,{
+      responseType:'text'
+    }));
+  }
+
+  UpdateRecord(obj:any){
+    return  (this.http.put(`${this.baseUrl}updateemployee`,obj,{
+      responseType:'text'
+    }));
+  }
+
+}
